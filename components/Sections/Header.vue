a1f8b61087ffc77397bed3f0e2b1fa9425ad3178
Freyam Mehta <frey77mehta@gmail.com>
2022-08-24 17:35:23 +0530

    Basic Details

---
+++ components/Sections/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <header>
+    <div
+      class="
+        container
+        mx-auto
+        px-8
+        py-4
+        md:px-12 md:py-10
+        flex
+        items-center
+        min-h-screen
+        max-w-6xl
+      "
+    >
+      <main v-if="about">
+        <h1 class="text-6xl md:text-7xl lg:text-8xl font-light">
+          {{ about.name }}
+        </h1>
+        <h2
+          class="
+            text-6xl
+            md:text-7xl
+            lg:text-8xl
+            font-bold
+            text-gray-500
+            dark:text-gray-400
+          "
+        >
+          {{ about.title }}
+        </h2>
+        <p class="mt-4 max-w-2xl text-gray-500 dark:text-gray-400 leading-1">
+          {{ about.bio }}
+        </p>
+      </main>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      about: {}
+    };
+  },
+  created() {
+    this.getAbout();
+  },
+  methods: {
+    async getAbout() {
+      const about = await this.$content('about').fetch();
+
+      this.about = about;
+    }
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Header />
+    <about />
+    <skill />
+    <experience />
+    <modal />
+    <project />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+import Header from '@/components/Sections/Header.vue';
+import About from '@/components/Sections/About.vue';
+import Skill from '@/components/Sections/Skill.vue';
+import Experience from '@/components/Sections/Experience.vue';
+import Modal from '@/components/Widgets/Modal.vue';
+import Project from '@/components/Sections/Project.vue';
+
+export default {
+  components: {
+    Header,
+    About,
+    Skill,
+    Experience,
+    Modal,
+    Project
+  },
+  mounted() {
+    this.closeMenu();
+  },
+  methods: mapActions({
+    closeMenu: 'closeMenu'
+  })
+};
+</script>
